0838654f6beb6a207041336f43907b8d7b03c4f0
Manish Kumar <itsmenishan.12@gmail.com>
2026-04-21 18:21:00 +0530

    Completed Parity Checker

--- src/main/java/org/example/PARITY_BIT_UDP/Client.java
+++ src/main/java/org/example/PARITY_BIT_UDP/Client.java
@@ -1,17 +1,43 @@
 package org.example.PARITY_BIT_UDP;
 
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try{
             DatagramSocket  socket = new DatagramSocket();
             InetAddress add = InetAddress.getByName("localhost");
+            int port = 5000;
 
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter Data in binary : ");
+            String data = sc.nextLine();
 
+            System.out.print("Do you want to send given data Enter 0 or parity data enter 1 : ");
+            int option = sc.nextInt();
 
+            if(option == 1){
+                byte[] arr = getValueWithParityAdded(data);
+                DatagramPacket sendPacket = new DatagramPacket(arr,arr.length,add,port);
+                socket.send(sendPacket);
+            }else{
+                byte[] arr = data.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(arr,arr.length,add,port);
+                socket.send(sendPacket);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static  byte[] getValueWithParityAdded (String data){
+        StringBuilder sb = new StringBuilder(data);
+        int counter =0;
+        for(int i = 0 ; i<data.length(); i++){
+            if(data.charAt(i) == '1') counter++;
+        }
+        sb.append(counter%2 == 0 ? '0' : '1' );
+        return  sb.toString().getBytes();
     }
 }
--- src/main/java/org/example/PARITY_BIT_UDP/Server.java
+++ src/main/java/org/example/PARITY_BIT_UDP/Server.java
@@ -5,19 +5,21 @@ public class Server {
     public static void main(String[] args) {
         try{
             DatagramSocket socket = new DatagramSocket(5000);
-            byte[] recByte = new byte[1024];
-            DatagramPacket recPacket = new DatagramPacket(recByte,recByte.length);
-            socket.receive(recPacket);
+            while(true) {
+                byte[] recByte = new byte[1024];
+                DatagramPacket recPacket = new DatagramPacket(recByte, recByte.length);
+                socket.receive(recPacket);
 
-            String str = new String(recPacket.getData(),0,recPacket.getLength());
+                String str = new String(recPacket.getData(), 0, recPacket.getLength());
 
-            int counter = 0;
-            for(int i = 0 ; i<str.length(); i++){
-                char c = str.charAt(i);
-                if(c == '1') counter++;
+                int counter = 0;
+                for (int i = 0; i < str.length(); i++) {
+                    char c = str.charAt(i);
+                    if (c == '1') counter++;
+                }
+                if (counter % 2 == 0) System.out.println("Correct Data Received : "+str);
+                else System.err.println("Invalid Data Received : "+str);
             }
-            if(counter%2 == 0) System.out.println("Correct Data Received...");
-            else System.err.println("Invalid Data Received...");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
